251130b2c1471052c6574450268cd84069838b18
shakhinn <nik.shakhin@gmail.com>
2020-09-08 00:20:40 +0300

    aisd_lb

---
+++ AISD_LB1/main.cpp
@@ -0,0 +1,27 @@
+#include "structs.h"
+#include "algo.h"
+#include "inputoutput.h"
+
+int main(int argc, char** argv) {
+    string expression = Cuinput(argc, argv);
+    if(expression == "HELP"){
+        return 0;
+    }
+    cout<<"Вы ввели: "<< expression<<endl;
+    bool result = CheckString(expression);
+    if(result){
+        cout<<"\033[1;32mSUCCESS\033[0m\n";
+    } else{
+        cout<<"\033[1;31mNOT_CORRECT\033[0m\n";
+    }
+    if(argc > 1){
+        if(result) {
+            ofstream outfile(optarg, ios::app);
+            outfile << "SUCCESS\n";
+        } else{
+            ofstream outfile(optarg, ios::app);
+            outfile << "NOT_CORRECT\n";
+        }
+    }
+    return 0;
+}
