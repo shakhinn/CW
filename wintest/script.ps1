a12cce691e844e84a7d02d6b7a591e1e2a2ec2e5
Никита Шахин <54898661+shakhinn@users.noreply.github.com>
2020-10-10 23:00:28 +0300

    Add files via upload

---
+++ wintest/script.ps1
@@ -0,0 +1,34 @@
+Write-Host "test 1"
+./AISD_LB2 -f tests/test.txt
+Write-Host "test 2"
+./AISD_LB2 -f tests/test1.txt
+Write-Host "test 3"
+./AISD_LB2 -f tests/test2.txt
+Write-Host "test 4"
+./AISD_LB2 -f tests/test3.txt
+Write-Host "test 5"
+./AISD_LB2 -f tests/test4.txt
+Write-Host "test 6"
+./AISD_LB2 -f tests/test5.txt
+Write-Host "test 7"
+./AISD_LB2 -f tests/test6.txt
+Write-Host "test 8"
+./AISD_LB2 -f tests/test7.txt
+Write-Host "test 9"
+./AISD_LB2 -f tests/test8.txt
+Write-Host "test 10"
+./AISD_LB2 -f tests/test9.txt
+Write-Host "test 11"
+./AISD_LB2 -f tests/test10.txt
+Write-Host "test 12"
+./AISD_LB2 -f tests/test11.txt
+Write-Host "test 13"
+./AISD_LB2 -f tests/test12.txt
+Write-Host "test 14"
+./AISD_LB2 -f tests/test13.txt
+Write-Host "test 15"
+./AISD_LB2 -f tests/test14.txt
+Write-Host "test 16"
+./AISD_LB2 -f tests/test15.txt
+
+
