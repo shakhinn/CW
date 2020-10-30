b085b1a27dcd86384f6ca98f4063be1f55a93782
shakhinn <nik.shakhin@gmail.com>
2020-10-30 13:26:35 +0300

    fixed

--- aisd_lb3/main.cpp
+++ aisd_lb3/main.cpp
@@ -8,27 +8,30 @@ int main (int argc, char** argv){
         return 0;
     }
     string copy = string(input);
-    int pos = 0;
-    BinaryTree tree = BinaryTree(copy, pos);
-    cout<<input<<endl;
-    if(tree.checkBST()) {
-        cout << "Tree is BST\n";
-        if (argc > 1) {
-            ofstream outfile(optarg, ios::app);
-            outfile << "Tree is BST\n";
+    BinaryTree tree = BinaryTree(copy);
+    if(!tree.isErr() && copy.empty()) {
+        cout<<input<<endl;
+        if (tree.checkBST()) {
+            cout << "Tree is BST\n";
+            if (argc > 1) {
+                ofstream outfile(optarg, ios::app);
+                outfile << "Tree is BST\n";
+            }
+        } else if (tree.checkPiramid()) {
+            cout << "Tree is a piramid\n";
+            if (argc > 1) {
+                ofstream outfile(optarg, ios::app);
+                outfile << "Tree is a piramid\n";
+            }
+        } else {
+            cout << "Tree is not BST or Piramid\n";
+            if (argc > 1) {
+                ofstream outfile(optarg, ios::app);
+                outfile << "Tree is not BST or Piramid\n";
+            }
         }
-    }else if(tree.checkPiramid()){
-        cout<<"Tree is a piramid\n";
-        if (argc > 1) {
-            ofstream outfile(optarg, ios::app);
-            outfile << "Tree is a piramid\n";
-        }
-    }else{
-        cout<<"Tree is not BST or Piramid\n";
-        if (argc > 1) {
-            ofstream outfile(optarg, ios::app);
-            outfile << "Tree is not BST or Piramid\n";
-        }
+    } else{
+        cout<<"error\n"<<input<<" is not a Tree or correct tree\n";
     }
     return 0;
 }
